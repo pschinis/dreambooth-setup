59c83f32e7f3c0936320be6a7446693596b207e0
Philip Schinis <p.schinis@gmail.com>
2022-12-09 13:24:23 -0500

    initial commit

---
+++ setup_script.sh
@@ -0,0 +1,6 @@
+pip install -U -qq git+https://github.com/huggingface/diffusers.git
+pip install -qq accelerate tensorboard transformers ftfy gradio
+pip install -qq "ipywidgets>=7,<8"
+pip install -qq bitsandbytes
+pip install -U --pre triton
+pip install -q https://github.com/TheLastBen/fast-stable-diffusion/raw/main/precompiled/A100/xformers-0.0.13.dev0-py3-none-any.whl
